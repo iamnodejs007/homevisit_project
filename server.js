8cbff6044537065241923b488bd4a75fd2e6480e
Jess Blokland <jess.blokland@yahoo.com>
2016-07-18 07:25:58 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+//Initialize npm packages
+
+var express = require("express");
+
+var app = express();
+
+app.use(express.static('public'));
+
+//GET TOUR DATA FROM DATABASE AND SHOW IT TO READER
+
+app.get
+
+
+//SUBMIT A NEW DATA ENTRY, A NEW TOUR 
+
+
+
+app.post 
+
+//UPDATE TOUR DATA BY ADDING A COMMENT
+
+app.post
+
+
+
+//DELETE 
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function ()  {
+    console.log ("Server is listening!");
+    });
