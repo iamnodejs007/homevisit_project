43195decdf6d8d130282dc5b13871e26103f20a7
Jess Blokland <jess.blokland@yahoo.com>
2016-08-18 08:22:24 +0000

    changing to environment variable for mongo db

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ app.use(express.static('public'));
 
 //CONNECT MONGO DB FOR DEV AND PRODUCTION/HEROKU ENVIRONMENTS
 
-mongoose.connect ("mongodb://Jess:jessme@ds161485.mlab.com:61485/homevisit");
-// mongoose.connect(process.env.DATABASEURL || "mongodb://localhost/tours");
+
+mongoose.connect(process.env.DATABASEURL || "mongodb://localhost/tours");
 
 var tourSchema = new Schema ({
     
